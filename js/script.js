839c653e3359779656bb65fc8b1dfc917256b0ae
GuhfelipeBEZ <167260711+GuhfelipeBEZ@users.noreply.github.com>
2024-05-14 20:02:18 -0300

    update JS e css

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 const toggleTheme = document.getElementById("toggleTheme");
-/* */ 
 const rootHtml = document.documentElement;
-
 const accordionHeaders = document.querySelectorAll(".accordion-header");
- 
 const menuLinks = document.querySelectorAll(".menu-link");
- /* moda o tema de claro para escuro*/
-function changTheme() {
 
+ /* moda o tema de claro para escuro*/
+function changTheme () {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
     currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-Theme", "dark"); /* codigo que controla o tema claro e escuro */
@@ -17,4 +14,23 @@ function changTheme() {
 }
 
     toggleTheme.addEventListener("click", changTheme);
+ // fim função mudar tema do site
 
+ accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => { 
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active"); 
+        
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+        
+    })
+ })
+ // fim da função de click no accordion 
+
+ menuLinks.forEach(item => {
+    item.addEventListener("click", () =>{
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+ })
+ //fim da função menu link
